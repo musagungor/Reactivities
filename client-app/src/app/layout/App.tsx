73070fcc58c86c3bc41542ef0ec7a5f62833fdf0
MUSA GUNGOR <musagungor@MUSAs-MacBook-Pro.local>
2020-01-22 21:13:47 +0300

    react componente class

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
-import "./App.css";
 import axios from "axios";
 import { Header, Icon, List } from "semantic-ui-react";
+import { IActivity } from "../models/activity";
 
-class App extends Component {
-  state = {
-    values: []
+interface IState {
+  activities: IActivity[]
+}
+
+class App extends Component <{},IState> {
+  readonly state: IState  = {
+    activities: []
   };
 
   componentDidMount() {
-    axios.get("http://localhost:5000/api/values").then(response => {
+    axios.get<IActivity[]>("http://localhost:5000/api/activities").then(response => {
       this.setState({
-        values: response.data
+        activities: response.data
       });
     });
   }
@@ -24,8 +28,8 @@ class App extends Component {
           <Header.Content>Reactivities</Header.Content>
         </Header>
         <List>
-          {this.state.values.map((value: any) => (             
-            <List.Item key={value.id}>{value.name}</List.Item>
+          {this.state.activities.map((activity) => (             
+            <List.Item key={activity.id}>{activity.title}</List.Item>
           ))}
         </List>
       </div>
