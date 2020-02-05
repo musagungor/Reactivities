72a20bf6373fded91ca7ed8020b32f09a8cb1531
m.gungor <musa.gungor@groupama.com.tr>
2020-02-05 07:58:23 +0300

    axios commands implemented

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect, Fragment } from "react";
-import axios from "axios";
 import { Container } from "semantic-ui-react";
 import { IActivity } from "../models/activity";
 import { NavBar } from "../../features/nav/NavBar";
 import { ActivityDashboard } from "../../features/activities/dashboard/ActivityDashboard";
+import agent from "../api/agent";
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
@@ -18,33 +18,40 @@ const App = () => {
     setEditMode(false);
   };
 
-  const handleOpenCreateForm=()=>{
+  const handleOpenCreateForm = () => {
     setSelectedActivity(null);
     setEditMode(true);
   }
 
-  const handleCreateactivity = (activity:IActivity) => {
-    setActivities([...activities,activity]);
-    setSelectedActivity(activity);
-    setEditMode(false);
+  const handleCreateactivity = (activity: IActivity) => {
+    agent.Activities.create(activity).then(() => {
+      setActivities([...activities, activity]);
+      setSelectedActivity(activity);
+      setEditMode(false);
+    })
   }
 
-  const handleEditActivity = (activity:IActivity) => {
-    setActivities([...activities.filter(a=>a.id!==activity.id),activity]);
-    setSelectedActivity(activity);
-    setEditMode(false);
+  const handleEditActivity = (activity: IActivity) => {
+    agent.Activities.update(activity).then(() => {
+      setActivities([...activities.filter(a => a.id !== activity.id), activity]);
+      setSelectedActivity(activity);
+      setEditMode(false);
+    })
+
   }
 
-  const handleDeleteActivity = (id:string) => {
-    setActivities([...activities.filter(a=>a.id !== id)]);
+  const handleDeleteActivity = (id: string) => {
+    agent.Activities.delete(id).then(() => {
+      setActivities([...activities.filter(a => a.id !== id)]);
+    })
+
   }
 
   useEffect(() => {
-    axios
-      .get<IActivity[]>("http://localhost:5000/api/activities")
+    agent.Activities.list()
       .then(response => {
-        let activities : IActivity[]=[];
-        response.data.forEach(activity=>{
+        let activities: IActivity[] = [];
+        response.forEach((activity) => {
           activity.date = activity.date.split('.')[0];
           activities.push(activity);
         });
@@ -54,18 +61,18 @@ const App = () => {
 
   return (
     <Fragment>
-      <NavBar openCreateForm = {handleOpenCreateForm} />
+      <NavBar openCreateForm={handleOpenCreateForm} />
       <Container className="margin-top-7">
         <ActivityDashboard
           activities={activities}
           selectActivity={handleSelectActivity}
           selectedActivity={selectedActivity}
-          editMode = {editMode}
-          setEditMode = {setEditMode}
-          setSelectedActivity = {setSelectedActivity}
-          createActivity = {handleCreateactivity}
-          editActivity = {handleEditActivity}
-          deleteActivity = {handleDeleteActivity}
+          editMode={editMode}
+          setEditMode={setEditMode}
+          setSelectedActivity={setSelectedActivity}
+          createActivity={handleCreateactivity}
+          editActivity={handleEditActivity}
+          deleteActivity={handleDeleteActivity}
         />
       </Container>
     </Fragment>
